566c86c1115b2421856ec207f4138a1748224c4c
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-11 16:15:03 +0200

    init commit with structure's squeleton

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode> {/* StrictMode = tool for highlighting potential problems */}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
