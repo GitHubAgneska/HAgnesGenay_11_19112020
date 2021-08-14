a17ad98e6ca56f19aa1972af0ca7f0edf6c6b81c
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-14 18:22:38 +0200

    base pages - style

--- src/components/containers/Rental.js
+++ src/components/containers/Rental.js
@@ -3,7 +3,7 @@ import RentalInfos from '../elements/RentalInfos'
 const Rental =  () => {
     
     return (
-        <section className="rentalPage-wrapper">
+        <section className="rentalPage-wrapper page">
             <RentalInfos />
         </section>
     )
