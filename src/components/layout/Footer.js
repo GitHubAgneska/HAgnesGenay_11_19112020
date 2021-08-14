368d5f8fcd28f5f48a59b4dd25ec09009baa2fc0
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-14 18:44:02 +0200

    base pages - responsive ok

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,14 +1,13 @@
 import { Component } from 'react';
 import  logo_white  from '../../assets/logos/logo_white.png'
 
-
 export class Footer extends Component {
     render() {
         return(
             <div className="footer-wrapper">
                 <footer id="footer" className="footer" role="contentinfo">
-                    <div id="footer__logo-wrapper" className="footer__logo-wrapper" tabIndex="0">
-                        <a href="#home" aria-label="Kasa Home page"><img src={logo_white} alt="logo" className="main-logo"/></a>
+                    <div className="footer__logo-wrapper" tabIndex="0">
+                        <a href="/home" aria-label="Kasa Home page"><img src={logo_white} alt="logo" className="main-logo"/></a>
                     </div>
                     <div className="footer__credentials">
                         <p>© 2020 Kasa. All rights reserved</p>
