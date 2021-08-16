79af341c808550e46be7d2546e0e518793fee1bf
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-16 12:14:08 +0200

    about  page - style/responsive

--- src/components/elements/CollapsingBlock.js
+++ src/components/elements/CollapsingBlock.js
@@ -1,4 +1,7 @@
 
+import caret_down from '../../assets/logos/caret_down.png'
+import caret_up from '../../assets/logos/caret_up.png'
+
 let blockTitle = 'placeholder'
 let blockContent = 'Lorem ipsum dolor si amet'
 
@@ -6,10 +9,13 @@ const CollapsingBlock = () => {
 
     return (
         <div className="collapsBlock collapsBlock-wrapper">
-            <div className="collapsBlock--header">
+            <div className="collapsBlock__header">
                 <h5>{blockTitle}</h5>
+                <div className="collapsBlock__header__caret-wrapper">
+                    <img src={caret_down} alt="caret logo open"></img>
+                </div>
             </div>
-            <div className="collapsBlock--body">
+            <div className="collapsBlock__body">
                 <p>{blockContent}</p>
             </div>
             
