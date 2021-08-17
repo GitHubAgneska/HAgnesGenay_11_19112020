686a46496d452c6bc1716c86670df3981dffe91b
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-17 10:44:32 +0200

    logic - fetch base ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, {Fragment, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header  from './components/layout/Header'
 import Footer from './components/layout/Footer'
@@ -8,12 +8,39 @@ import About from './components/containers/About'
 import Rental from './components/containers/Rental'
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 
+const apiUrl = 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json'
+const myHeaders = new Headers();
+const myRequest = new Request( apiUrl, {
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors', //  mode: 'no-cors' ? 
+    cache: 'default',
+    credentials: 'include'
+});
 
 // each top-level feature declares its own route
 // Fragment used here because Router can't have multiple children
-class App extends Component {
+export default function App() {
+  
+  const [ data, setData ] = useState(null);
+  const [ loading, setLoading ] = useState(true);
+  const [ error, setError ] = useState(null);
 
-  render() {
+  useEffect(()=> {
+    fetch(myRequest)
+      .then(response => {
+        if (response.ok) { return response.json() } throw response })
+      .then(data => { setData(data);console.log(data) })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setError(error);
+      })
+      .finally(()=> { setLoading(false)})
+  }, [])
+
+  if (loading) return 'Loading..';
+  if (error) return 'Error!';
+
     return (
       <div className="App">
         <div className="container">
@@ -35,7 +62,7 @@ class App extends Component {
         </div>
       </div>
     );
-  }
 }
 
-export default App;
+
+
--- src/components/containers/Home.js
+++ src/components/containers/Home.js
@@ -2,8 +2,11 @@ import React from 'react'
 import HomeBanner from '../elements/HomeBanner'
 import Gallery from '../elements/RentalsGallery'
 
+
 export class Home extends React.Component {
+    
     render() {
+
         return (
             <div id="homepage" className="homepage-wrapper page">
                 <HomeBanner />
