7e767d3d55bfed928d8d65e3697b7101b265362d
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-22 06:45:14 +0200

    carousel - logic + style ok

--- src/components/elements/Carousel.js
+++ src/components/elements/Carousel.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Children, useState } from 'react';
+import React, { Fragment } from 'react';
+import { useState } from 'react';
 import CarouselItem from "./CarouselItem";
 
 const Carousel = (props) => {
 /* const Carousel = ({children}) => { */
     const [activeIndex, setActiveIndex] = useState(0);
+    const hasOnlyOnePicture = props.pictures.length === 1;
 
     const updateIndex = (newIndex) => {
-        /* if ( newIndex < 0 ) { newIndex = Children.count(children) ; }
+        /* using React.Children
+        if ( newIndex < 0 ) { newIndex = Children.count(children) ; }
         else if (newIndex > Children.count(children) ){ newIndex = 0; } */
         if ( newIndex < 0 ) { newIndex = (props.pictures).length -1 ; }
-        else if (newIndex > (props.pictures).length ){ newIndex = 0; }
+        else if (newIndex > (props.pictures).length -1 ){ newIndex = 0; }
 
         setActiveIndex(newIndex);
         console.log('CURRENT INDEX===', newIndex)
@@ -18,20 +20,30 @@ const Carousel = (props) => {
 
     return (
         <div className="carousel-wrapper">
-            <div className="carousel-wrapper__inner">
-                <div className="controls-wrapper">
-                    <div className="control prev" onClick={() => { updateIndex(activeIndex - 1) }}>&#10094;</div>
-                    <div className="control next" onClick={() => { updateIndex(activeIndex + 1) }}>&#10095;</div>
-                </div>
-                <div className="pic-wrapper" style={{ transform: `translateX(-${activeIndex* 100}%)`}}>
+
+            { !hasOnlyOnePicture? (
+
+                <div className="carousel-wrapper__inner">
+                
+                    <div className="controls-wrapper">
+                        <div className="control prev" onClick={() => { updateIndex(activeIndex - 1) }}>&#10094;</div>
+                        <div className="control next" onClick={() => { updateIndex(activeIndex + 1) }}>&#10095;</div>
+                    </div>
                     
-                    {/* {Children.map(children, (child, index) => {
-                        return React.cloneElement(child, {width: "100%"})
-                    })} */}
-            
-                    {props.pictures.map((pic) => (<CarouselItem pic={pic} key={Math.random()} /> ))}
+                    <div className="pic-wrapper" style={{ transform: `translateX(-${activeIndex* 100}%)`}}>
+                        {props.pictures.map((pic) => (<CarouselItem pic={pic} key={Math.random()} /> ))}
+                        {/* using React.Children - {Children.map(children, (child, index) => { return React.cloneElement(child, {width: "100%"}) })} */}
+                    </div>
+
                 </div>
-            </div>
+                ): 
+                <div className="carousel-wrapper__inner">
+                    <div className="pic-wrapper"  style={{ transform: `translateX(-${activeIndex* 100}%)`}}>
+                        <CarouselItem pic={props.pictures[0]} key={Math.random()} />
+                    </div>
+                </div>
+            }
+
         </div>
     )
 }
