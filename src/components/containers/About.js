9bc23b2354a45a47d42628cdb218e66631937661
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-27 07:18:40 +0200

    wrapping-up : about page - adding json for content

--- src/components/containers/About.js
+++ src/components/containers/About.js
@@ -1,14 +1,14 @@
 import CollapsingBlock from '../elements/CollapsingBlock'
+import about from '../../data/about.json';
 
 const About =  () => {
-    let sections = ['Fiabilité', 'Respect', 'Service', 'Responsabilité'];
-    
+
     return (
         <section className="aboutPage-wrapper page">
             <div className="banner-pic banner-pic__about"></div>
             <div className="about-sections">
-                { sections.map(i => (
-                    <CollapsingBlock key={i} blockTitle={i} description="lorem ipsum"/>
+                { about.about.map(i => (
+                    <CollapsingBlock key={i.title} blockTitle={i.title} description={i.description} />
                 ))}
                 
             </div>
