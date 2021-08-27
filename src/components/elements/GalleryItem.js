338c9f26488d02ef8a0b9b3b0e2198bf76de4192
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-27 07:26:45 +0200

    wrapping-up : turning nav comp to functional component

--- src/components/elements/GalleryItem.js
+++ src/components/elements/GalleryItem.js
@@ -1,5 +1,5 @@
 import { useHistory } from "react-router-dom";
-import React, {useState } from 'react';
+import React from 'react';
 
 export const GalleryItem =({rental}) =>  {
 
--- src/components/elements/Nav.js
+++ src/components/elements/Nav.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Nav extends Component {
-    render() {
-        return (
-            <div className='topnav-wrapper'>
-                <nav className='top-nav'>
-                    <li>
-                        <a className='nav-link link-home' href='/home'>Accueil</a>
-                    </li>
-                    <li>
-                        <a className='nav-link link-about' href='/about'>A propos</a>
-                    </li>
-                </nav>
-            </div>
-        )
-    }
+const Nav = () => {
+
+    return (
+        <div className='topnav-wrapper'>
+            <nav className='top-nav'>
+                <li>
+                    <a className='nav-link link-home' href='/home'>Accueil</a>
+                </li>
+                <li>
+                    <a className='nav-link link-about' href='/about'>A propos</a>
+                </li>
+            </nav>
+        </div>
+    )
 }
 
 export default Nav
