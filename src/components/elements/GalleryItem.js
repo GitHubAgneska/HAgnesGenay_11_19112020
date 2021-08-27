dce989a8cd797d602a419fe1b8aa8b72457e4ecc
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-08-27 06:54:45 +0200

    wrapping-up : galleryItem commenting out unused state props

--- src/components/elements/GalleryItem.js
+++ src/components/elements/GalleryItem.js
@@ -5,7 +5,7 @@ export const GalleryItem =({rental}) =>  {
 
     const history = useHistory();
     function handleClick(id) { history.push("/rental/"+id); console.log(id)}
-    const [ imgLoaded, setImgLoaded ] = useState(false);
+    /* const [ imgLoaded, setImgLoaded ] = useState(false); */
 
     /* better alternative to event handler :  binding event in class constructor */
     /*  ( -> to avoid multi rendering of a different callback everytime the event is called )*/
